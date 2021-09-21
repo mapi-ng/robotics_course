d2e78c56105298e89bde3d4d11df1c69c7489bf4
Marcin Pilch <marcinpilch4@gmail.com>
2021-09-21 22:31:05 +0200

    Added AMCL test script

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -3,8 +3,6 @@ xterm -e  " roscore" &
 sleep 3
 xterm -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/worlds/world_proj.world" &
 sleep 3
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find maps)/maps/map.yaml " &
 sleep 3
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-sleep 3
-xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch " &
