630607f60958eba9440db226cb4cb6964afa1dc0
Marcin Pilch <marcinpilch4@gmail.com>
2021-09-21 21:34:26 +0200

    test_slam

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,7 +2,11 @@
 xterm -e  " roscore" &
 sleep 3
 xterm -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/worlds/world_proj.world" &
-sleep 1
-#xterm  -e  " roslaunch slam_gmapping turtlebot_world.launch " &
-#sleep 5
-xterm  -e  " rosrun rviz rviz" 
+sleep 3
+xterm  -e  " rosrun rviz rviz" &
+sleep 3
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+sleep 3
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 3
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch " &
